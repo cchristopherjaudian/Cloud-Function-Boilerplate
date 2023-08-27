f5afba7a29de53e8c28e25a3968030ac226f1560
cliljs <calilchristopher052997@gmail.com>
2023-08-27 17:01:48 +0800

    feat: added middlewares build: installed jsonwebtoken feat: added class for firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,12 +10,18 @@
 import express from 'express';
 import * as logger from 'firebase-functions/logger';
 import CloudFunctions, { THttpsFunction } from './lib/cloud-functions';
+import RouteMiddleware from './middleware/response-handlers';
+
 const app = express();
 
 app.get('/', (request, response) => {
   logger.info('Hello logs!', { structuredData: true });
   response.json('Hello from Firebase!');
 });
+
+// route middlewares e.g(not found, error handlers)
+app.use(new RouteMiddleware().errorResponse);
+app.use(new RouteMiddleware().notFound);
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
--- functions/src/lib/custom-errors/class-errors.ts
+++ functions/src/lib/custom-errors/class-errors.ts
@@ -1,5 +1,5 @@
 import httpStatus from 'http-status';
-import ResponseCodes from '../../commons/response-codes';
+import ResponseCodes from '../../../commons/response-codes';
 import BaseError from './base-error';
 
 class NotFoundError extends BaseError {
