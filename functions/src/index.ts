bf9410b72324911e8ec92fd0a33f35b64edb6313
Calil Christopher Jaudian <cchristopherjaudian@Calils-MacBook-Air.local>
2023-12-10 00:56:42 +0800

    refactor: refactor src/index added initialized routes, renamed cf name

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,10 +3,14 @@ import cors from "cors";
 import CloudFunctions from "./lib/cloud-functions";
 import ErrorMiddleware from "./middleware/error-middleware";
 import { HttpsFunction } from "firebase-functions/v2/https";
+import routes from "./routes";
 
 const app = express();
 
 app.use(cors());
+
+// initialized service routes
+app.use(routes);
 
 // route middlewares e.g(not found, error handlers)
 app.use(new ErrorMiddleware().errorResponse);
@@ -14,6 +18,6 @@ app.use(new ErrorMiddleware().notFound);
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
-export const v2 = CloudFunctions.instance
+export const cf_boilerplate = CloudFunctions.instance
   .withRuntime()
   .handlerV2(<HttpsFunction>(<unknown>app));
